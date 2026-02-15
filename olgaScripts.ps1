8bee99aa3f4e2a079d0fc5952537cd78ed678154
Nik <tabola1000@gmail.com>
2026-02-15 18:50:48 +0300

    Check my script try2

--- olgaScripts.ps1
+++ olgaScripts.ps1
@@ -1,1 +1,14 @@
-Write-Host "Hi bro"
+# Изменяем яркость на 100% (Визуальный тест "света")
+$brightness = 100
+$delay = 0
+$display = Get-CimInstance -Namespace root/WMI -ClassName WmiMonitorBrightnessMethods
+Invoke-CimMethod $display -MethodName WmiSetBrightness -Arguments @{Brightness = $brightness; Timeout = $delay}
+
+# Ждем 5 секунд
+Start-Sleep -Seconds 5
+
+# Возвращаем яркость на 50%
+Invoke-CimMethod $display -MethodName WmiSetBrightness -Arguments @{Brightness = 50; Timeout = $delay}
+
+# Создаем текстовый лог для проверки в файлах
+Add-Content -Path "C:\remote_test_log.txt" -Value "Code executed successfully at $(Get-Date)"
