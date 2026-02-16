c06021cc65e8934f2f6492e71bb1963d513d09d1
Nik <tabola1000@gmail.com>
2026-02-16 15:45:35 +0300

    Added other message for two waies

--- olgaScripts.ps1
+++ olgaScripts.ps1
@@ -4,7 +4,7 @@ Add-Type -AssemblyName System.Windows.Forms
 # Находим устройство тачпада по части имени
 $touchpad = Get-PnpDevice | Where-Object { $_.FriendlyName -like "*TouchPad*" -or $_.Description -like "*TouchPad*" }
 
-[System.Windows.Forms.MessageBox]::Show("«Оля, скинь мне описание неполадки в Viber. Подожди, пока я всё исправлю в этом файле, потом запусти его снова. Текущий скрипт отключает тачпад. Нажми 'Окей' для отключения и выхода;", "Жди!")
+[System.Windows.Forms.MessageBox]::Show("«Оля, скинь мне описание неполадки в Viber. Подожди, пока я всё исправлю в этом файле, потом запусти его снова. Текущий скрипт отключает тачпад. Нажми 'Окей' для отключения и выхода;", "Жди!", "OK", "Information")
 
 
 if ($touchpad) {
@@ -14,6 +14,12 @@ if ($touchpad) {
     # Лог для проверки
     Add-Content -Path "$env:TEMP\touchpad_check.txt" -Value "Тачпад отключен удаленно: $(Get-Date)"
     Write-Host "Тачпад ($($touchpad.FriendlyName)) деактивирован."
+
+    [System.Windows.Forms.MessageBox]::Show("Отключение тачпада прошло успешно, никаких препятствий на пути не возникло!", "Задачу выпонил!", "OK", "Information")
 } else {
     Write-Warning "Устройство тачпада не найдено."
+    [System.Windows.Forms.MessageBox]::Show("Что-то пошло не так! Нужно углубится в тему!", "Задача не выполнена!", "OK", "Error")
 }
+
+
+exit
